64c485df241d3cbb9d4a6867aa4ec116d1b1ed29
Saurabh Anand <sanand@everfi.com>
2019-11-06 13:32:03 +0530

    [Homepage] Adding new floder structure

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import { Navbar, Button, Collapse, Container, Row, Col } from 'react-bootstrap';
+import React from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
-import Typography from './Typography/Typography';
-import Drawer from './components/Drawer/Drawer';
+import PageHeader from './Layout/PageHeader/PageHeader';
+import HomePage from './Layout/HomePage';
+import PageFooter from './Layout/PageFooter/PageFooter';
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const showComponents = function(e) {
-    document.querySelector('#main-content').focus();
-  }
   return (
-    <div id="differentlyAbled">
-      <header>
-        <Navbar bg="dark" variant="dark" fixed="top">
-          <Navbar.Brand tabIndex="0" onClick={showComponents} onKeyDown={showComponents}>
-            <img
-              alt=""
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            {' Skip to main content'}
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-              <Button variant="link"
-                onClick={() => setOpen(!open)}
-                aria-controls="nav-drawer"
-                aria-expanded={open}
-              >
-                <img
-                  alt="ham"
-                  src={logo}
-                  width="30"
-                  height="30"
-                  className="d-inline-block align-top"
-                />
-              </Button>
-          </Navbar.Collapse>
-        </Navbar>
-        <Collapse in={open}>
-          <div id="nav-drawer">
-            <Drawer/>
-          </div>
-        </Collapse>
-      </header>
-      <main id="main-content" tabIndex="-1">
-        <Container>
-          <Typography />
-        </Container>
-      </main>
-    </div>
+    <Container fluid>
+      <PageHeader mainContainer='#main'/>
+      <HomePage />
+      <PageFooter />
+    </Container>
   );
 }
 
---
+++ src/Layout/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+class HomePage extends React.Component {
+    render(props) {
+        return (
+            <Container id="main" tabIndex="-1">
+                <Row>
+                    <Col sm={4}>
+
+                    </Col>
+                    <Col sm={8}>
+                        
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+export default HomePage;
