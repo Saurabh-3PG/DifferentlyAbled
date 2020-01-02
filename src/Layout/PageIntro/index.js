8f68e94a926293f670587d468e2056155bc0f74a
Saurabh Anand <sanand@everfi.com>
2020-01-02 15:03:04 +0530

    Added introduction page

--- src/Layout/PageIntro/index.js
+++ src/Layout/PageIntro/index.js
@@ -11,14 +11,40 @@ class PageIntro extends React.Component {
     }
     render(props) {
         let heading = <h1>{this.props.heading}</h1>;
+        let sec_heading = <h2>{this.props.sec_heading}</h2>;
         let subheading = <div><b><i>{this.props.subheading}</i></b></div>;
         let paragraph = <p>{this.props.paragraph}</p>;
+        let list, table = null
+        if (this.props.list){
+            list = <ul>{
+                this.props.list.map(list => {
+                    return (
+                        <li key={list.item}>{list.item}</li>
+                    )
+                }
+                )}</ul>
+        }
+        if (this.props.table) {
+            list = <table>{
+                this.props.table.map(table => {
+                    return (
+                        <tr className={table.class}>
+                            <td>{table.guideline}</td>
+                            <td>{table.summary}</td>
+                        </tr>
+                    )
+                }
+                )}</table>
+        }
         return (
             <Row>
                 <Col>
                     {this.props.heading ? heading : null}
+                    {this.props.sec_heading ? sec_heading : null}
                     {this.props.subheading ? subheading : null}
                     {this.props.paragraph ? paragraph : null}
+                    {list}
+                    {table}
                 </Col>
             </Row>
         );
