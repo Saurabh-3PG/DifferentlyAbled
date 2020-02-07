48ee927de69d4b8c8aa2aa9909999631372d9bf9
Saurabh Anand <sanand@everfi.com>
2020-02-07 16:27:43 +0530

    Updated new images and some more fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import PageIntro from './Layout/PageHeader/PageHeader';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  console.log('Test result For App.js');
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  console.log('Test result For PageIntro.js');
+  it('PageIntro is rendered as h1', () => {
+    const PageInto = shallow(<PageIntro />);
+    expect(PageInto.find('h1').length).toEqual(1);
+  });
+})
