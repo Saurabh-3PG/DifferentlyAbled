c98f1d3e5fd9c6d51f7940ed4c4a319fa5ba8488
Saurabh Anand <sanand@everfi.com>
2020-02-18 14:34:46 +0530

    Adding build files

---
+++ build/precache-manifest.6153c35520df020e4b2bc26325617fb7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce405a1557ed2a9af307ede942c953f5",
+    "url": "/saurabhanand3390/differentlyabled/index.html"
+  },
+  {
+    "revision": "f680da52f212f3b7daa1",
+    "url": "/saurabhanand3390/differentlyabled/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dca9ab3a183aded89bd4",
+    "url": "/saurabhanand3390/differentlyabled/static/css/main.cb384671.chunk.css"
+  },
+  {
+    "revision": "f680da52f212f3b7daa1",
+    "url": "/saurabhanand3390/differentlyabled/static/js/2.cf126413.chunk.js"
+  },
+  {
+    "revision": "dca9ab3a183aded89bd4",
+    "url": "/saurabhanand3390/differentlyabled/static/js/main.f11dedeb.chunk.js"
+  },
+  {
+    "revision": "5d897bb63380043d0a22",
+    "url": "/saurabhanand3390/differentlyabled/static/js/runtime-main.8ad0adf0.js"
+  },
+  {
+    "revision": "3bc6ed99c3e145dd1377bdd5ebfd9824",
+    "url": "/saurabhanand3390/differentlyabled/static/media/Html.3bc6ed99.png"
+  },
+  {
+    "revision": "5662956a50b079adbe26fe15065d1390",
+    "url": "/saurabhanand3390/differentlyabled/static/media/Html.5662956a.png"
+  },
+  {
+    "revision": "83839809a7fa716a8aa277de59b5233d",
+    "url": "/saurabhanand3390/differentlyabled/static/media/Html.83839809.png"
+  },
+  {
+    "revision": "9b2481cc251e5b23a5a92d708a86a26f",
+    "url": "/saurabhanand3390/differentlyabled/static/media/Html.9b2481cc.png"
+  },
+  {
+    "revision": "9e881256ab87682becf093e375c25e0f",
+    "url": "/saurabhanand3390/differentlyabled/static/media/Html.9e881256.png"
+  },
+  {
+    "revision": "2ba9417da4b9ba5609a7205608e61310",
+    "url": "/saurabhanand3390/differentlyabled/static/media/ReactJs.2ba9417d.png"
+  },
+  {
+    "revision": "5a16bcd09a87558da0a88034ee43fe04",
+    "url": "/saurabhanand3390/differentlyabled/static/media/ReactJs.5a16bcd0.png"
+  },
+  {
+    "revision": "c700095fc705c13d238e20bcb924e3a5",
+    "url": "/saurabhanand3390/differentlyabled/static/media/ReactJs.c700095f.png"
+  },
+  {
+    "revision": "d082ae204d81a2bd2411cb0981324ed1",
+    "url": "/saurabhanand3390/differentlyabled/static/media/ReactJs.d082ae20.png"
+  },
+  {
+    "revision": "e656581059eea2e0d7faf59f1793b097",
+    "url": "/saurabhanand3390/differentlyabled/static/media/ReactJs.e6565810.png"
+  },
+  {
+    "revision": "53c007614868f0eeb60ff188119b9bc8",
+    "url": "/saurabhanand3390/differentlyabled/static/media/RenderedHTML.53c00761.png"
+  },
+  {
+    "revision": "6b179d24615fc988b32a8afbd6895ac5",
+    "url": "/saurabhanand3390/differentlyabled/static/media/RenderedHTML.6b179d24.png"
+  },
+  {
+    "revision": "8aadef569b2d418bbc12e1353488fb51",
+    "url": "/saurabhanand3390/differentlyabled/static/media/RenderedHTML.8aadef56.png"
+  },
+  {
+    "revision": "ac743bc7396c551013515d303deaa74d",
+    "url": "/saurabhanand3390/differentlyabled/static/media/RenderedHTML.ac743bc7.png"
+  },
+  {
+    "revision": "f5eb6c3d4ca2ea811632ba556a2aac8a",
+    "url": "/saurabhanand3390/differentlyabled/static/media/RenderedHTML.f5eb6c3d.png"
+  },
+  {
+    "revision": "c7fbda65d9ea5701b18be17d3856922a",
+    "url": "/saurabhanand3390/differentlyabled/static/media/heading-using-div.c7fbda65.png"
+  },
+  {
+    "revision": "156b102c07d9f3d73f1f86960c7abf4c",
+    "url": "/saurabhanand3390/differentlyabled/static/media/heading-using-hTag.156b102c.png"
+  },
+  {
+    "revision": "18071d1778e9e916e21703396899c161",
+    "url": "/saurabhanand3390/differentlyabled/static/media/list-hTag.18071d17.png"
+  },
+  {
+    "revision": "5a87e305295a34670cb9b39aa5304b2e",
+    "url": "/saurabhanand3390/differentlyabled/static/media/paragraph.5a87e305.png"
+  }
+]);
